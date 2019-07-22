3301672bf3652cfbcebaa86f3ad747e20f0883ca
ylisovskyi <lisovskyi.yurii@gmail.com>
2019-07-22 19:00:41 +0300

    Drum-pad initial implementation

--- drumpad.py
+++ drumpad.py
@@ -1,5 +1,6 @@
 from pyfirmata import Arduino
 import pyglet
+import time
 
 # Set-up Arduino and media player
 arduino = Arduino("COM3")
@@ -16,15 +17,20 @@ sounds = [
     '2.wav',
     '3.wav'
 ]
+print('Importing sounds...')
 for sound in sounds:
     source = pyglet.media.load('drum_sounds/%s' % sound, streaming=False)
     player.queue(source)
 
+player.volume = 0.5
 player.volume = float(arduino.analog[volume_num].read()) / 1000
 
 while True:
     if (arduino.digital[drum_num].read()):
         player.play()  # Drum beat
+        time.sleep(player.source.duration)
+        player.pause()
+        player.seek(0)
     player.volume = float(arduino.analog[volume_num].read()) / 1000  # Adjust volume
     if (arduino.digital[switch_num].read()):
         player.next_source()  # Choose next drum sound
